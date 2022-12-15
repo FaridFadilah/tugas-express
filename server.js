cb28faefc4b16ccc8dabc0de5d1a825b981646ab
FaridFadilah <faridfadillah29@gmail.com>
2022-12-15 16:11:30 +0700

    setup db

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
-const app = express()
+const db = require('./app/models')
+const Role = db.role
 
 corsOptions = {
   origin: 'http://localhost:8081'
@@ -14,6 +15,26 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.get('/', (req, res) => {
   res.json({ message: 'welcome to bezkoder application'})
 })
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and resync DB')
+  initial()
+})
+
+function initial(){
+  Role.create({
+    id: 1,
+    name: 'user'
+  })
+  Role.create({
+    id: 2,
+    name: 'moderator'
+  })
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
